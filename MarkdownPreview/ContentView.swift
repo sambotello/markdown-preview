f7618bd658132031b53f8db60e39a5c48c7cc00d
sambotello <sambotello.dev@gmail.com>
2026-07-17 10:45:27 -0600

    Initial Commit

---
+++ MarkdownPreview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarkdownPreview
+//
+//  Created by Samuel Botello on 17/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarkdownPreview/MarkdownPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownPreviewApp.swift
+//  MarkdownPreview
+//
+//  Created by Samuel Botello on 17/07/26.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
